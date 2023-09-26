b0d21d0085ce5aaa16497eaf994c6bd04819d7fe
marco <marcogutierrez@gmail.com>
2023-09-25 22:29:26 -0500

    inicio de CL1

---
+++ src/test/java/com/marco/demo/Cl1MarcoGutierrezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marco.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1MarcoGutierrezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
